a5e66f8e7cc71aa981427b656999e71ca0679b87
Salaimuniselvam <salaimuniselvam7@gmail.com>
2021-07-24 00:47:58 +0530

    finding best burger using burger api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-const name =document.querySelector('.viewport')
+const burgerApi =document.querySelector('.viewport')
 const button=document.querySelector('.button')
 
 function getDisplay () {
-    fetch
+    fetch("https://my-burger-api.herokuapp.com/burgers", {
+	"method": "GET",
+})
+
+.then(response =>response.json())
+.then(data => {
+    const number = Math.random()*data.length;
+    const roundedNumber = Math.floor(number); 
+
+    const output = data[roundedNumber].name
+    console.log(output);
+    burgerApi.innerHTML=output;   
+})
+
+}
+
+button.addEventListener('click',getDisplay)
